e59254ff72264935eca752ef6758abe5e4e92e43
Dhriti <103201952+Dhriti-1312@users.noreply.github.com>
2024-05-01 10:11:50 +0530

    added init and npm i

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log("server will run here");
 console.log(date());
+
+//create a new repo
+// add .readme .gitignore files
+// git clone url of repo
+// open project in vscode (cd repoFolder)
+// npm init -y
+// add new file app.js with some console statements
+// npm i
+// git add .
+// git commit -m "message to add file"
+// git push
